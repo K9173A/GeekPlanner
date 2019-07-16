0721bfeb5933c3f8a947b450d7f5d87f56722c7a
K9173A <K9173A@gmail.com>
2019-07-16 23:09:16 +0300

    [FIX] Fixed indentation.

--- GeekPlanner/mainapp/views.py
+++ GeekPlanner/mainapp/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 
@@ -7,6 +6,11 @@ class IndexView(TemplateView):
     template_name = 'mainapp/index.html'
 
     def get_context_data(self, **kwargs):
+        """
+        Returns context data for displaying the object.
+        :param kwargs: additional key-value arguments.
+        :return: context data for the template.
+        """
         context = super(IndexView, self).get_context_data(**kwargs)
         context['title'] = 'Главная'
         return context
--- GeekPlanner/plannerapp/urls.py
+++ GeekPlanner/plannerapp/urls.py
@@ -6,12 +6,44 @@ import plannerapp.views as plannerapp
 app_name = 'plannerapp'
 
 urlpatterns = [
-    path('projects/', plannerapp.ProjectListView.as_view(), name='projects'),
-    path('create_project/', plannerapp.ProjectCreateView.as_view(), name='create_project'),
-    path('update_project/<int:pk>/', plannerapp.ProjectUpdateView.as_view(), name='update_project'),
-    path('delete_project/<int:pk>/', plannerapp.ProjectDeleteView.as_view(), name='delete_project'),
-    path('project_details/<int:pk>/', plannerapp.ProjectDetailView.as_view(), name='project_details'),
-    path('create_card/<int:project_pk>/', plannerapp.CardCreateView.as_view(), name='create_card'),
-    path('delete_card/<int:pk>/project/<int:project_pk>/', plannerapp.CardDeleteView.as_view(), name='delete_card'),
-    path('update_card/<int:pk>/project/<int:project_pk>/', plannerapp.CardUpdateView.as_view(), name='update_card'),
+    path(
+        'projects/',
+        plannerapp.ProjectListView.as_view(),
+        name='projects'
+    ),
+    path(
+        'create_project/',
+        plannerapp.ProjectCreateView.as_view(),
+        name='create_project'
+    ),
+    path(
+        'update_project/<int:pk>/',
+        plannerapp.ProjectUpdateView.as_view(),
+        name='update_project'
+    ),
+    path(
+        'delete_project/<int:pk>/',
+        plannerapp.ProjectDeleteView.as_view(),
+        name='delete_project'
+    ),
+    path(
+        'project_details/<int:pk>/',
+        plannerapp.ProjectDetailView.as_view(),
+        name='project_details'
+    ),
+    path(
+        'create_card/<int:project_pk>/',
+        plannerapp.CardCreateView.as_view(),
+        name='create_card'
+    ),
+    path(
+        'delete_card/<int:pk>/project/<int:project_pk>/',
+        plannerapp.CardDeleteView.as_view(),
+        name='delete_card'
+    ),
+    path(
+        'update_card/<int:pk>/project/<int:project_pk>/',
+        plannerapp.CardUpdateView.as_view(),
+        name='update_card'
+    ),
 ]
